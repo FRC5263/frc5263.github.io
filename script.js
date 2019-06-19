fd1185ed94c26ea1f904e0cdac944e9119c65809
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-06-19 18:04:42 -0400

    new website mockup

--- script.js
+++ script.js
@@ -1,104 +1,83 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-function getData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        let response = yield fetch('https://api.keyvalue.xyz/74a033e0/resetrobotics', {
-            method: "GET"
-        });
-        let json = yield response.json();
-        return json;
-    });
-}
-function postData(data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        fetch('https://api.keyvalue.xyz/74a033e0/resetrobotics', {
-            method: "POST",
-            body: JSON.stringify(data)
-        });
-    });
-}
-const toDo = document.getElementById('toDo');
-const inProgress = document.getElementById('inProgress');
-const done = document.getElementById('done');
-const ideas = document.getElementById('ideas');
-function putData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        let data = yield getData();
-        toDo.innerHTML = '';
-        inProgress.innerHTML = '';
-        done.innerHTML = '';
-        ideas.innerHTML = '';
-        data.toDo.forEach((item, index) => {
-            let div = document.createElement('div');
-            div.innerHTML = item;
-            div.classList.add('post');
-            toDo.appendChild(div);
-        });
-        data.inProgress.forEach((item, index) => {
-            let div = document.createElement('div');
-            div.innerHTML = item;
-            inProgress.appendChild(div);
-            div.classList.add('post');
-        });
-        data.done.forEach((item, index) => {
-            let div = document.createElement('div');
-            div.innerHTML = item;
-            div.classList.add('post');
-            done.appendChild(div);
-        });
-        data.ideas.forEach((item, index) => {
-            let div = document.createElement('div');
-            div.innerHTML = item;
-            div.classList.add('post');
-            ideas.appendChild(div);
+document.addEventListener('DOMContentLoaded', () => {
+
+    const burger = document.querySelector('.navbar-burger');
+    const menu = document.getElementById('navMenu');
+
+
+    [burger, menu].forEach(element => element.addEventListener('click', () => {
+        if (burger.classList.contains('is-active')) {
+            burger.classList.remove('is-active');
+        } else {
+            burger.classList.add('is-active');
+        }
+        if (menu.classList.contains('is-active')) {
+            menu.classList.remove('is-active');
+        } else {
+            menu.classList.add('is-active');
+        }
+    }));
+
+    let navLinks = document.querySelectorAll('.navbar-item');
+    navLinks.forEach(link => {
+        const href = link.getAttribute('href');
+        const target = document.querySelector(href);
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            target.scrollIntoView({ behavior: "smooth" });
         });
     });
-}
-let toDoButton = document.getElementById('toDoSubmit');
-toDoButton.addEventListener('click', (event) => __awaiter(this, void 0, void 0, function* () {
-    event.preventDefault();
-    let value = document.getElementById('toDoInput');
-    let data = yield getData();
-    data.toDo.unshift(value.value);
-    value.value = '';
-    yield postData(data);
-    setTimeout(() => putData(), 500);
-}));
-let inProgressButton = document.getElementById('inProgressSubmit');
-inProgressButton.addEventListener('click', (event) => __awaiter(this, void 0, void 0, function* () {
-    event.preventDefault();
-    let value = document.getElementById('inProgressInput');
-    let data = yield getData();
-    data.inProgress.unshift(value.value);
-    value.value = '';
-    yield postData(data);
-    setTimeout(() => putData(), 500);
-}));
-let doneButton = document.getElementById('doneSubmit');
-doneButton.addEventListener('click', (event) => __awaiter(this, void 0, void 0, function* () {
-    event.preventDefault();
-    let value = document.getElementById('doneInput');
-    let data = yield getData();
-    data.done.unshift(value.value);
-    value.value = '';
-    yield postData(data);
-    setTimeout(() => putData(), 500);
-}));
-let ideasButton = document.getElementById('ideasSubmit');
-ideasButton.addEventListener('click', (event) => __awaiter(this, void 0, void 0, function* () {
-    event.preventDefault();
-    let value = document.getElementById('ideasInput');
-    let data = yield getData();
-    data.ideas.unshift(value.value);
-    value.value = '';
-    yield postData(data);
-    setTimeout(() => putData(), 500);
-}));
-putData();
-//# sourceMappingURL=script.js.map
+
+
+    const galleryImages = [
+        'team1.jpeg',
+        'team2.jpeg',
+        'team3.jpeg',
+        'team4.jpeg',
+        'team5.jpg',
+    ];
+
+
+    const home = document.getElementById('home');
+    home.style.backgroundRepeat = 'no-repeat';
+    home.style.backgroundSize = 'cover';
+    home.style.backgroundPosition = 'center';
+
+    function setBackground(pastImage) {
+        let image = pastImage + 1;
+        if(pastImage === 4){
+            image = 0;
+        }
+        home.style.backgroundImage = `url("media/${galleryImages[image]}")`;
+        console.log('hi', image, galleryImages[image])
+
+        setTimeout(() => setBackground(image), 5000);
+    }
+
+    setBackground(0)
+
+    // const gallery = document.getElementById('gallery');
+    
+    // const galleryImages = [
+    //     'team1.jpeg',
+    //     'team2.jpeg',
+    //     'team3.jpeg',
+    //     'team4.jpeg',
+    //     'team5.jpeg',
+    // ];
+
+    // const previousChildren = gallery.childNodes;
+    // gallery.innerHTML = '';
+
+    // galleryImages.forEach(imageSrc => {
+    //     const img = document.createElement('img');
+    //     img.setAttribute('src', `/media/${imageSrc}`);
+    //     img.style.position = 'absolute';
+    //     gallery.appendChild(img);
+    // })
+
+    // previousChildren.forEach(child => {
+    //     gallery.appendChild(child)
+    // })
+
+
+});
